7526e0f866a95cd6c04c0189677acd9be89990b3
David Kim <davekim88@gmail.com>
2022-12-09 12:51:14 -0800

    changes to pagesAbout

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -7,4 +7,4 @@ export const PageAbout = () => {
             <p>Saepe vitae deserunt cupiditate vel reiciendis adipisci quasi. At, dolore qui, saepe similique id repellat ipsam sapiente repellendus commodi deleniti natus itaque hic temporibus nam nobis tempora enim suscipit quas!</p>
         </section>
     );
-};
+}; test
