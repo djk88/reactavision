841337857c2c944e0cdb5958ade3a81aab5ca006
lisa1204pm <dlee560@my.bcit.ca>
2023-01-06 13:18:22 -0800

    adding trailers and doing the about page

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -1,12 +1,48 @@
 import React from 'react'
+import team from '../image/team-first.png'
 
 export const PageAbout = () => {
     return (
-        <div>
-            <section>
-                <h2>About Page</h2>
-                <p>Saepe vitae deserunt cupiditate vel reiciendis adipisci quasi. At, dolore qui, saepe similique id repellat ipsam sapiente repellendus commodi deleniti natus itaque hic temporibus nam nobis tempora enim suscipit quas!</p>
-            </section>
+        <div className="movie-page">
+        <div className="container">
+            <div className="header">
+                <h1 className="heading">About Page</h1>
+            </div>
+            <div>
+                <p>Breakthrough Movie App </p>
+                <p>This website is a movie Introduction service with over 45,000 movies, and featuring content from every major Hollywood studio. We give fans of film an easy way to discover new content that is completely free.</p>
+            
+                <h1>Our Values</h1>
+                <p>We care deeply about our culture. The five values below serve as the guiding principles for everything we do.</p>
+            </div>
+            <div>
+                <section>
+                <img src ={team}></img>
+                <h3>Team First</h3>
+                <p>We are most successful when we work together and perform as one team. We empower each other, prioritize team success, and trust our teammates to do the same.</p>
+                </section>
+                <section>
+                <img src ={team}></img>
+                <h3>Own It</h3>
+                <p>We are all owners of the work that we do individually and as a team. We believe brilliant ideas can come from all corners of our organization, and encourage thought leadership from everyone at Tubi.</p>
+                </section>
+                <section>
+                <img src ={team}></img>
+                <h3>Own It</h3>
+                <p>We are all owners of the work that we do individually and as a team. We believe brilliant ideas can come from all corners of our organization, and encourage thought leadership from everyone at Tubi.</p>
+                </section>
+                <section>
+                <img src ={team}></img>
+                <h3>Own It</h3>
+                <p>We are all owners of the work that we do individually and as a team. We believe brilliant ideas can come from all corners of our organization, and encourage thought leadership from everyone at Tubi.</p>
+                </section>
+                <section>
+                <img src ={team}></img>
+                <h3>Own It</h3>
+                <p>We are all owners of the work that we do individually and as a team. We believe brilliant ideas can come from all corners of our organization, and encourage thought leadership from everyone at Tubi.</p>
+                </section>
+            </div>
         </div>
+    </div>
     );
 }; 
