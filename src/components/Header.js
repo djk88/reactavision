4badee133af44bc385402d842be70f78e415b24b
lisa1204pm <dlee560@my.bcit.ca>
2023-01-13 12:17:44 -0800

    add tmdb logo and message comment hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+// import { useState } from 'react';
+// import { SidebarData } from './SidebarData';
 
 export const Header = () => {
+    // const [sidebar, setSidebar] = useState(false);
+    // const showSidebar = () => setSidebar(!sidebar);
+
     return (
         <header>
             <div className="container">
@@ -9,6 +13,24 @@ export const Header = () => {
                     <div className="brand">
                         <Link to="/"><span className='logo'>REACT</span>AVISION</Link>
                     </div>
+                    {/* <div className='navbar'>
+                    <a href="#head-nav" className="header-btn" id="header-btn" onClick={showSidebar}><span className="hidden"></span></a>
+                    </div>
+                    
+                    <nav id='navigation' className={sidebar ? 'nav-menu active' : 'nave-menu'}>
+                        <ul className='nav-menu-items'>
+                            {SidebarData.map((item, index) => {
+                                return(
+                                    <li key={index} className={item.cName}>
+                                        <Link to={item.path}>
+                                            {item.icon}
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </nav> */}
                     <ul className="nav-links">
                         <li>
                             <Link to="/">Home</Link>
@@ -28,3 +50,5 @@ export const Header = () => {
         </header>
     );
 };
+
+export default Header;
---
+++ src/components/SidebarData.js
@@ -0,0 +1,23 @@
+export const SidebarData = [
+    {
+    title: 'home',
+    path:'/',
+    cName: 'nav-text'
+    },
+    {
+    title: 'About',
+    path:'/PageAbout',
+    cName: 'nav-text'
+    },
+    {
+    title: 'Favorite List',
+    path:'/PageFavoriteList',
+    cName: 'nav-text'
+    },
+    {
+    title: 'Search',
+    path:'/PageSearch',
+    cName: 'nav-text'
+    }
+]
+
